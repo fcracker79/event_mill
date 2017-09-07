b38421cb37a4d0d999d06338910a37513b77d208
fcracker79 <fcracker79@gmail.com>
2017-09-07 23:32:53 +0200

    Tests

--- test/test.py
+++ test/test.py
@@ -1,7 +1,11 @@
 import unittest
 
 
-class MyTest(unittest.TestCase):
+class ImportTest(unittest.TestCase):
     def test_should_pass(self):
+        from event_mill.event import declarations
+        from event_mill.event_source import declarations
+        from event_mill.projection import declarations
+        from event_mill.reactor import declarations
+        from event_mill import exceptions
         pass
-
