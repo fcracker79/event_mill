f16d079789244ba27e4020363bc07256c50c9a15
daddinuz <daddinuz@gmail.com>
2017-09-25 00:12:24 +0200

    event source refactor

--- event_mill/event_source/declarations.py
+++ event_mill/event_source/declarations.py
@@ -1,16 +1,31 @@
 import abc
+import collections
 import typing
 
 from event_mill.event.declarations import Event
 
 
-class Rewindable(metaclass=abc.ABCMeta):
+class EventIterable(collections.Iterable, metaclass=abc.ABCMeta):
     @abc.abstractmethod
-    def rewind(self):
+    def __enter__(self):
+        pass  # pragma: no cover
+
+    @abc.abstractmethod
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass  # pragma: no cover
+
+    @abc.abstractmethod
+    def __iter__(self) -> typing.Iterable[Event]:
         pass  # pragma: no cover
 
 
 class EventSource(metaclass=abc.ABCMeta):
     @abc.abstractmethod
-    def get_events(self, limit: int) -> typing.Iterable[Event]:
+    def get_events(self, limit: int) -> EventIterable:
+        pass  # pragma: no cover
+
+
+class Rewindable(metaclass=abc.ABCMeta):
+    @abc.abstractmethod
+    def rewind(self):
         pass  # pragma: no cover
--- event_mill/event_source/support.py
+++ event_mill/event_source/support.py
@@ -2,7 +2,37 @@ import abc
 import typing
 
 from event_mill.event.declarations import Event
-from event_mill.event_source.declarations import EventSource, Rewindable
+from event_mill.event_source.declarations import EventIterable, EventSource, Rewindable
+
+
+class EmptyEventIterable(EventIterable):
+    def __enter__(self):
+        pass
+
+    def __iter__(self) -> typing.Iterable[Event]:
+        return ()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass
+
+
+class DelegateEventIterable(EventIterable):
+    def __init__(self, delegate: EventIterable, on_exit_callback: typing.Callable[[], None]):
+        self._events = None
+        self._delegate = delegate
+        self._on_exit_callback = on_exit_callback
+
+    def __enter__(self) -> EventIterable:
+        self._events = self._delegate.__enter__()
+        return self
+
+    def __iter__(self) -> typing.Iterable[Event]:
+        return self._events
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if exc_type is None and exc_val is None and exc_tb is None:
+            self._on_exit_callback()
+        return self._delegate.__exit__(exc_type, exc_val, exc_tb)
 
 
 class DelegateRewindableEventSource(EventSource, Rewindable, metaclass=abc.ABCMeta):
@@ -14,56 +44,34 @@ class DelegateRewindableEventSource(EventSource, Rewindable, metaclass=abc.ABCMe
         pass  # pragma: no cover
 
     @abc.abstractmethod
-    def _get_events_and_update_height(self, limit: int) -> typing.Iterable[Event]:
+    def _get_height(self) -> int:
         pass  # pragma: no cover
 
     @abc.abstractmethod
-    def _add_events_and_update_height(self, events: typing.Iterable[Event]):
+    def _get_events(self, limit: int) -> EventIterable:
         pass  # pragma: no cover
 
-    def get_events(self, limit: int) -> typing.Iterable[Event]:
-        events = None
-        while not events:
-            events = self._get_events_and_update_height(limit)
-            if not events:
-                with self._delegate.get_events(limit) as events:
-                    self._add_events_and_update_height(events)
+    @abc.abstractmethod
+    def _get_received_events_height(self) -> int:
+        pass  # pragma: no cover
+
+    @abc.abstractmethod
+    def _save_received_events(self, events: typing.Iterable[Event]):
+        pass  # pragma: no cover
+
+    def _get_events_and_update_height(self, limit: int) -> typing.Optional[DelegateEventIterable]:
+        received_events_height = self._get_received_events_height()
+        if self._get_height() == received_events_height:
+            return None
+        return DelegateEventIterable(self._get_events(limit), lambda: self._set_height(received_events_height))
+
+    def get_events(self, limit: int) -> EventIterable:
+        events = self._get_events_and_update_height(limit)
+        if events is None:
+            with self._delegate.get_events(limit) as events:
+                self._save_received_events(events)
+            events = EmptyEventIterable()
         return events
 
     def rewind(self):
         self._set_height(0)
-
-
-class DelegateContextEventSource(EventSource, metaclass=abc.ABCMeta):
-    def __init__(self, delegate: EventSource):
-        self._delegate = delegate
-
-    @abc.abstractmethod
-    def _before_get_events(self):
-        pass  # pragma: no cover
-
-    @abc.abstractmethod
-    def _after_get_events(self):
-        pass  # pragma: no cover
-
-    @abc.abstractmethod
-    def _after_failed_get_events(self, exception: Exception):
-        pass  # pragma: no cover
-
-    class _ScopedIterable:
-        def __init__(self, context: 'DelegateContextEventSource', generate_events_function: callable):
-            self._context = context
-            self._generate_events_function = generate_events_function
-
-        def __enter__(self):
-            return self._context._before_get_events()
-
-        def __exit__(self, exc_type, exc_val, exc_tb):
-            return self._context._after_failed_get_events(exc_val) if exc_val \
-                else self._context._after_get_events()
-
-        def __iter__(self):
-            return self._generate_events_function()
-
-    def get_events(self, limit: int) -> typing.Iterable[Event]:
-        return self._ScopedIterable(self, lambda: self._delegate.get_events(limit))
